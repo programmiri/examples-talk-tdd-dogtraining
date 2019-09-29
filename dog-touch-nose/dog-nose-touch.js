e68e87794f6504ef98db639767bbd77e71b222e0
programmiri <programmiri@gmail.com>
2019-09-29 18:20:36 +0200

    step 12 - code

--- dog-touch-nose/dog-nose-touch.js
+++ dog-touch-nose/dog-nose-touch.js
@@ -5,13 +5,18 @@ class Dog {
 
 	#rewards = ["Frolic", "Ball", "Playtime"];
 	#punishments = ["Petting on the Head", "Hugging", "Loud cheering"];
+	#fingerLookingRewarded = 0;
 
 	touch() {
+		if (this.#fingerLookingRewarded >= 5) {
+			return "🐶💡 *touch*";
+		}
 		return "❓❓🐶❓❓";
 	}
 
 	looksAtFingers(treat) {
 		if (this.#rewards.includes(treat)) {
+			this.#fingerLookingRewarded++;
 			return "🐶🎉 *rewarded*";
 		}
 		if (this.#punishments.includes(treat)) {
